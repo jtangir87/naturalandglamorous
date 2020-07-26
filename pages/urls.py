8a0f7ef6ea9747e61398eca6eaf601e23deb9a66
Jared Tangir <jaredtangir@Jareds-MacBook-Pro-3.local>
2020-07-26 11:20:49 -0400

    contact form and vlog titles

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, contact_us
 
 urlpatterns = [
     path('', home, name="home"),
+    path('contact-us', contact_us, name="contact_us"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from blog.models import VlogPost
 
+from django.template.loader import get_template
+from django.http import JsonResponse
+from django.core.mail import send_mail
+
 # Create your views here.
 
 
@@ -9,3 +13,32 @@ def home(request):
     rec_vlogs = VlogPost.objects.filter(
         published=True).order_by('-created_at')
     return render(request, 'pages/home.html', {'rec_vlogs': rec_vlogs})
+
+
+def contact_us(request):
+    data = dict()
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        template = get_template('pages/contact_us.txt')
+        context = {
+            'name': name,
+            'email': email,
+            'subject': subject,
+            'message': message,
+        }
+        content = template.render(context)
+        send_mail(
+            "N&G Contact Form",
+            content,
+            "{}<{}>".format(name, email),
+            ['naturalandglamorous@gmail.com'],
+            fail_silently=False,
+        )
+        data["form_is_valid"] = True
+    else:
+        data["form_is_valid"] = False
+    return JsonResponse(data)
